b622add8823d15f65569d732fdc2626ae9cd6be0
Olatunde Bakre <ail>
2022-09-28 16:57:20 -0700

    Committed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,56 +1,32 @@
 #include "main.h"
-
 /**
- *evaluate_num - recursion loop
- *@num: num
- *@iterator: number to iterate
- *Return: return 1 or 0
- */
-
-int evaluate_num(int num, int iterator)
+* is_prime_number - returns true if the number is prime
+*@n: the number to check
+*
+*Return: true if the number is prime
+*/
+int is_prime_number(int n)
 {
+	int start = n / 2;
 
-if (iterator == num - 1)
-{
-return (1);
-}
-
-else if (num % iterator == 0)
-{
-return (0);
-}
-
-if (num % iterator != 0)
-{
-return (evaluate_num(num, iterator + 1));
-}
-
-return (0);
-
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, start));
 }
 
 /**
- *is_prime_number - evaluate prime or not
- *@num: number
- *Return: return 1 prime - return 0 otherwise
- */
+* is_prime - returns the 1 if n is prime
+* @n: number to be checked
+* @start: number to start checking from
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
 
-int is_prime_number(int num)
+int is_prime(int n, int start)
 {
-
-int iterator;
-
-iterator = 2;
-
-/* only greater than 2*/
-if (num < 2)
-{
-return (0);
+	if (start <= 1)
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return (is_prime(n, start - 1));
 }
-
-if (num == 2)
-{
-return (1);
-}
-
-return (evaluate_num(num, iterator));
